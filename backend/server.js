886152a678ba3dfaffa3e03bfd96832eeb2ef726
ponnanapavan <pavanponnana1@gmail.com>
2024-10-29 13:06:21 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -85,12 +85,11 @@ io.on('connection', (socket) => {
 
 
 
-if(process.env.NODE_ENV === 'Production'){
       app.use(express.static(path.join(__dirname, '/frontend/dist')))
       app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+            res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")) 
       })
-}
+
 
 // Attach io instance to the app
 app.io = io;
